a9a0bf40e762644cdb1b3a01d16a8e6e25b68825
Kee Ryun Park <kee@dhcp-206-87-135-111.ubcsecure.wireless.ubc.ca>
2018-12-01 13:16:31 -0800

    initial implementation of the Map component

--- lhd/src/App.js
+++ lhd/src/App.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
+
+import Map from './components/Map';
 
 class App extends Component{
     render (){
         return (
             <div>
                 TEST
+                <Map />
+                <Button variant="contained" color="primary">
+                    Hello World
+                </Button>
             </div>
         )
     }
--- lhd/src/components/Map.js
+++ lhd/src/components/Map.js
@@ -1,1 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class Map extends Component {
+    static defaultProps = {
+        center: {
+            lat: 49.2606,
+            lng: -123.2460
+        },
+        zoom: 11
+    };
+
+    render() {
+        return (
+            // Important! Always set the container height explicitly
+            <div style={{ height: '90vh', width: '80%' }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: "PASTE_YOUR_API_KEY" }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                >
+                    <AnyReactComponent
+                        lat={59.955413}
+                        lng={30.337844}
+                        text={'Kreyser Avrora'}
+                    />
+                </GoogleMapReact>
+            </div>
+        );
+    }
+}
+
+export default Map;
