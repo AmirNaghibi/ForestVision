f6bc8fe589d065cdef61a5f9d363b43a41e8eeee
jatinsuneja <s.jatin1903@gmail.com>
2018-12-01 13:02:17 -0800

    form + node modules

--- lhd/src/App.js
+++ lhd/src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Form from './components/Form';
 
 class App extends Component{
     render (){
         return (
             <div>
-                TEST
+                <Form/>
             </div>
         )
     }
