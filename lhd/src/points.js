ee69e3060c63d1a5921f8789285983461d15f8fc
Kee Ryun Park <kee@dhcp-206-87-135-111.ubcsecure.wireless.ubc.ca>
2018-12-01 13:55:05 -0800

    separated out sample data points

---
+++ lhd/src/points.js
@@ -0,0 +1,16 @@
+const points = [
+    {
+        location: [49.2606, -123.2455],
+        weight: 20
+    },
+    {
+        location: [49.2610, -123.2461],
+        weight: 30
+    },
+    {
+        location: [49.2608, -123.2460],
+        weight: 10
+    },
+];
+
+export default points;
