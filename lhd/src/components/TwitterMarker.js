6888459230070225f3afb7eba752d915fe4c63a5
Kee Ryun Park <kee@dhcp-206-87-135-111.ubcsecure.wireless.ubc.ca>
2018-12-01 17:21:43 -0800

    created TwitterMarker.js

---
+++ lhd/src/components/TwitterMarker.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Modal from '@material-ui/core/Modal';
+import { TwitterTweetEmbed } from 'react-twitter-embed';
+import { Whatshot } from '@material-ui/icons';
+
+
+class TwitterMarker extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            open: false
+        };
+
+        this.handleOpen = this.handleOpen.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    handleOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        const { tweetId } = this.props;
+
+        return (
+            <div>
+                <Whatshot onClick={this.handleOpen}>Open Modal</Whatshot>
+                <Modal
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description"
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                >
+                    <div  className="modal-div">
+                        <TwitterTweetEmbed tweetId={tweetId} />
+                    </div>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default TwitterMarker;
